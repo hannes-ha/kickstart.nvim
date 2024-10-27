b6d368b9eb40f7600afa8ea61a942d4465eba6fc
hannes-ha <63145826+hannes-ha@users.noreply.github.com>
2024-10-27 20:33:17 +0100

    fugitive diff binds

--- init.lua
+++ init.lua
@@ -106,6 +106,8 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- Ctrl-q to close current buffer
 vim.keymap.set('n', '<C-q>', ':bd<CR>')
 
+-- <leader> p to paste without yanking
+vim.keymap.set('x', '<leader>p', [["_dP]])
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
@@ -140,7 +142,6 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup {
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
-  'tpope/vim-fugitive',
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
