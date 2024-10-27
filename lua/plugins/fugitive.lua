45c22d696b14bca5970d53c3190e516856314f8c
hannes-ha <63145826+hannes-ha@users.noreply.github.com>
2024-10-27 20:45:26 +0100

    corret git diff command

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,7 +1,7 @@
 return {
   'tpope/vim-fugitive',
   config = function()
-    vim.keymap.set('n', '<leader>gd', ':Gdiff<CR>')
+    vim.keymap.set('n', '<leader>gd', ':Gvdiffsplit!<CR>')
     vim.keymap.set('n', '<leader>ml', '<cmd>diffget //2<CR>')
     vim.keymap.set('n', '<leader>mr', '<cmd>diffget //3<CR>')
   end,
