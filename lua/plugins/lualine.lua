00b6248a3e398417ff4fed5f3c9a916eeffda98d
hannes-ha <63145826+hannes-ha@users.noreply.github.com>
2024-12-10 17:34:52 +0100

    remove webicons

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,15 +1,14 @@
 return {
   'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
     require('lualine').setup {
-
       options = {
         fmt = string.lower, -- small letters in mode
-        theme = 'auto',
         component_separators = { left = '|', right = '|' }, -- remove pointy things
-        icons_enabled = false,
         section_separators = { left = '', right = '' },
+      },
+      sections = {
+        lualine_x = { 'filetype' }, -- hide encoding and file format
       },
     }
   end,
